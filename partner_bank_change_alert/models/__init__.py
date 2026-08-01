db4f84eaa1e208f6588f2c99f40a3b5c2e0c6cf9
Farhan Ashraf <ayesha@bizaffix.com>
2026-08-01 14:35:27 +0500

    [ADD] partner_bank_change_alert | Alert and Chatter Audit on Bank Account Changes (18.0)

---
+++ partner_bank_change_alert/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of partner_bank_change_alert. License: LGPL-3 <https://www.gnu.org/licenses/lgpl-3.0.html>.
+from . import res_partner_bank
+from . import res_config_settings
